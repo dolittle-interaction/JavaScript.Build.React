5b9f4c9637a2b9f9ecf8b3ad3bdc5aadaee9b96c
Sindre Wilting <sindrewilting@gmail.com>
2019-02-03 14:54:51 +0100

    Quick fix, temporary solution for wallaby

--- wallaby.js
+++ wallaby.js
@@ -1,21 +1,50 @@
-const DefinePlugin = require('webpack').DefinePlugin;
+//TODO: Rely on @dolittle/build/wallaby 
+// const wallaby = require('@dolittle/build/dist/wallaby/node')
+// module.exports = wallaby('.', () => {});
 
-const wallaby = require('@dolittle/build/wallaby')
+const babelConfigLoader = require('@dolittle/build/dist/babelConfigLoader').default;
+const babelConfig = babelConfigLoader('.');
+module.exports = function (sourceFolder, wallabySettingsCallback) {
+    if (sourceFolder === undefined || sourceFolder === null) sourceFolder = 'Features';
+    return (wallaby) => {
+        const babelCompiler = wallaby.compilers.babel(babelConfig);
+        let wallabyConfig = {
+            files: [
+                { pattern: 'node_modules/chai/chai.js', instrument: false },
+                { pattern: 'node_modules/chai-as-promised/chai-as-promised.js', instrument: false },
+                { pattern: 'node_modules/sinon/pkg/sinon.js', instrument: false },
+                { pattern: 'node_modules/sinon-chai/lib/sinon-chai.js', instrument: false },
+                { pattern: `${sourceFolder}/**/for_*/**/*.js`, ignore: true },
+                { pattern: `${sourceFolder}/**/dist/**/*.js`, ignore: true },
+                { pattern: `${sourceFolder}/**/*.js` }
+            ],
+            tests: [
+                { pattern: `${sourceFolder}/**/dist/**/for_*/**/*.js`, ignore: true },
+                { pattern: `${sourceFolder}/**/for_*/**/*.js` }
+            ],
+            testFramework: 'mocha',
+            compilers: {
+                '**/*.js*': babelCompiler
+            },
+            env: {
+                // Should not be node
+                type: 'node'
+            },
+            setup: () => {
+                global.expect = chai.expect;
+                let should = chai.should();
+                global.sinon = require('sinon');
+                let sinonChai = require('sinon-chai');
+                chai.use(sinonChai);
+                let sinonChaiInOrder = require('sinon-chai-in-order').default;
+                chai.use(sinonChaiInOrder);
+    
+                let winston = require('winston');
+                global.logger = winston.createLogger({});
+            }
+        };
+        if (typeof wallabySettingsCallback === 'function') wallabySettingsCallback(wallabyConfig);
 
-module.exports = (baseFolder, webpackPostprocessorCallback, wallabySetingsCallback) => {
-    if( !baseFolder ) baseFolder = 'Features';
-    return wallaby(baseFolder, webpack => {
-
-        webpack.module.rules = webpack.module.rules.concat([
-            { test: /\.html$/i, loader: 'html-loader' },
-            { test: /\.css$/i, issuer: [{ not: [{ test: /\.html$/i }] }], use: ['style-loader', 'css-loader'] },
-            { test: /\.css$/i, issuer: [{ test: /\.html$/i }], use: 'css-loader' },   
-        ]);
-
-        if( typeof webpackPostprocessorCallback == 'function' ) webpackPostprocessorCallback(webpack);
-
-    }, wallaby => {
-        if( typeof wallabySetingsCallback == 'function' ) wallabySetingsCallback(wallaby);
-    });
+        return wallabyConfig;
+    }
 };
-
